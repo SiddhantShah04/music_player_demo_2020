3afa81af024b92213470c0612b45122084155472
Supritkumar <supritk16@gmail.com>
2020-09-10 17:27:19 +0530

    play ,pause button dynamic change implemented

---
+++ script.js
@@ -0,0 +1,27 @@
+const music = document.querySelector('audio');
+const prevBtn= document.getElementById('prev');
+const playBtn= document.getElementById('play');
+const nextBtn= document.getElementById('next');
+
+//Che ck if playing
+let isPlaying = false;
+
+
+// play 
+function playSong(){
+    isPlaying = true;
+    playBtn.classList.replace('fa-play','fa-pause');
+    playBtn.setAttribute('title','Pause');
+    music.play();
+}
+
+// pause
+function pauseSong(){
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause','fa-play');
+    playBtn.setAttribute('title','Play');
+    music.pause();
+}
+
+// play or pause
+playBtn.addEventListener('click',()=>(isPlaying ? pauseSong() :playSong()))
