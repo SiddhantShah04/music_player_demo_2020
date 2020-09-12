919bbcdd1e6cae2dfd1bf7420d9ef0ec51b00f44
Supritkumar <supritk16@gmail.com>
2020-09-13 00:57:22 +0530

    song duration added and song running current time  and duration also added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const artist=document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer=document.getElementById('progress-container');
 const progress = document.getElementById('progress');
+const currentTimeEl=document.getElementById('current-time');
+const durationEl=document.getElementById('duration');
 const prevBtn= document.getElementById('prev');
 const playBtn= document.getElementById('play');
 const nextBtn= document.getElementById('next');
@@ -100,6 +102,25 @@ function updateProgessBar(e){
     
     const progressPercent=(currentTime/duration)*100;
     progress.style.width=`${progressPercent}%`;
+        // Calculate display for duration
+        const durationMinutes=Math.floor(duration/60);
+        let durationSeconds = Math.floor(duration%60);
+
+        if(durationSeconds < 10){
+            durationSeconds = `0${durationSeconds}`;
+        }
+        // Delay switching duration element to avoid NaN
+        if(durationSeconds){
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        // Calculate display for current time
+        const currentMinutes=Math.floor(currentTime/60);
+        let currentSeconds = Math.floor(currentTime%60);
+
+        if(currentSeconds < 10){
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent=`0${currentMinutes}:${currentSeconds}`;
     }
 }
 
