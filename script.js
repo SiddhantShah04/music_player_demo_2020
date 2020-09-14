c42328bcea9afb4f97be8b6811ad2686b16616ef
Supritkumar <supritk16@gmail.com>
2020-09-14 16:29:21 +0530

    set progress bar on current progress bar click

--- script.js
+++ script.js
@@ -124,9 +124,17 @@ function updateProgessBar(e){
     }
 }
 
+// Set progress bar
+function setProgressBar(e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const {duration}=music;
+    music.currentTime = (clickX/width)*duration;
+}
+
 // Event Listener
 prevBtn.addEventListener('click',prevSong);
 nextBtn.addEventListener('click',nextSong);
-
-
+music.addEventListener('ended',nextSong);
 music.addEventListener('timeupdate',updateProgessBar);
+progressContainer.addEventListener('click',setProgressBar);
